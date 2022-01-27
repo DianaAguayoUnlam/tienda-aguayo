0ed2ebf209cf54f5bbefd97e04eca28ddf7e3ac6
DianaAguayoUnlam <ddianaosi15@gmail.com>
2022-01-26 23:27:09 -0300

    Muestro un mensaje a partir de una prop llamada greeting
    Desafío 4.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'; 
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.js'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <NavBar/>
+      <ItemListContainer greeting='Bienvenido/a a Plantillas Webs'/>
+      <ItemListContainer greeting='Plantillas originales y atractivas'/>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,7 @@
+
+const ItemListContainer = (props) => {
+    return (<h2>{props.greeting}</h2>);
+  };
+  
+export default ItemListContainer;
+  
